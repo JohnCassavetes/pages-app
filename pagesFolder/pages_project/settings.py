65f290e2f291316cbffc10dd0bc705dd8089a220
Jeremy <jgautama7@gmail.com>
2021-11-22 16:37:00 +0700

    New updates for Heroku deployment

--- pagesFolder/pages_project/settings.py
+++ pagesFolder/pages_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False #should be True if still in producation
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
